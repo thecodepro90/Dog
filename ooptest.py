ec16b528ef4660be2f84593a124ced023e51f732
thecodepro90 <johnglopezzz@gmail.com>
2022-01-11 16:52:10 -0700

    test

--- ooptest.py
+++ ooptest.py
@@ -1,1 +1,11 @@
 
+class Person:
+    number_of_people = 0
+
+    def __init__(self, name):
+         self.name = name
+
+p1 = Person("tim")
+p2 = Person("jill")
+Person.number_of_people = 8
+print(p2.number_of_people)
