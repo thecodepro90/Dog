7cfe8ab3c239fd25227a0e70d00efa39ac62caa6
thecodepro90 <johnglopezzz@gmail.com>
2022-01-13 14:42:19 -0700

    deleted all of main

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-value = input("How many students: ")
-
 class Student:
     def __init__(self, name, age, grade):
         self.name = name
         self.age = age
         self.grade = grade
+
+    def get_age(self):
+        return age
+
+    def get_grade(self):
+        return grade
+
+    def get_name(self):
+        return name
+
+class Course:
+    def __init__(self, name, age, grade, max_students):
