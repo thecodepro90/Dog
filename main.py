555119993d9ddd8eff5c004e9808b2b481e282dd
thecodepro90 <johnglopezzz@gmail.com>
2022-01-13 15:44:06 -0700

    added set speak message

--- main.py
+++ main.py
@@ -5,23 +5,37 @@ class Pet:
         self.color = color
 
     def get_name(self):
-        return name
+        return self.name
 
     def get_age(self):
-        return age
+        return self.age
 
     def get_color(self):
-        return color
+        return self.color
+
+    def get_food(self):
+        return self.food
 
 class Dog(Pet):
-    def __init__(self, food):
-        self.food = food
+    def __init__(self, name, age, color, food, message):
+       super().__init__(name, age, color)
+       self.food = food
+       self.message = message
 
-    def bark(self):
-        return "Bark"
+    def speak(self):
+        message = self.message
+        return message
 
     def eat_food(self):
-        return "Munch Munch this is some good {self.food}"
+        print(f"Munch Munch this is some good {self.food}")
 
-    def set_food(self, food):
-        self .food = food
+    def set_food(self,food):
+        self.food = food
+
+    def set_speak_message(self, message):
+        self.message = message
+
+d = Dog("Tim", 12, "Blue", "Steak", "Bark")
+print(d.speak())
+d.set_speak_message("this is different")
+print(d.speak())
