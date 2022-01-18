041b377bacf906cf3c6cec9b2f25e22cf84000c7
thecodepro90 <johnglopezzz@gmail.com>
2022-01-17 17:30:54 -0700

    idk

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from path1 import *
+import path1
 
 class Dog_data:
     def __init__(self, name, age, color, dinner, lunch, breakfast):
@@ -28,44 +28,49 @@ class Dog_data:
 
 d = Dog_data("name", 1, "color", "food", "food", "food")
 
-print("hi im the Virtual_dog what would you like my name to be?")
-var = input("Enter: ")
-d.name = var
-print(f"{d.name}, thats a nice name!")
-print("What would you like my age to be?")
-var1 = input("Enter: ")
-d.age = int(var1)
 
-if d.age >= 10:
-    old = True
-elif d.age < 10:
-    old = False
 
-if old == True:
-    print(f"dang {d.age} is old!")
-elif old == False:
-    print(f"yay {d.age} is young!")
+def dogstart():
+    print("hi im the Virtual_dog what would you like my name to be?")
+    var = input("Enter: ")
+    d.name = var
+    print(f"{d.name}, thats a nice name!")
+    print("What would you like my age to be?")
+    var1 = input("Enter: ")
+    d.age = int(var1)
 
-print("Now that we're getting somewhere what would you like the color of my fur to be?")
-var2 = input("Enter: ")
-d.color = var2
-print(f"{d.color}, is a nice color!")
-print("ok a few more things, what would you like me to eat for dinner?")
-var3 = input("Enter: ")
-d.dinner = var3
-print("What do you want be to eat for lunch?")
-var4 = input("Enter: ")
-d.lunch = var4
-print("What do you want me to eat for breakfast?")
-var5 = input("Enter: ")
-d.breakfast = var5
-print(f"so for dinner ill have {d.dinner} and for lunch ill have {d.lunch} and for breakfast ill have {d.breakfast} ")
+    if d.age >= 10:
+        old = True
+    elif d.age < 10:
+        old = False
 
-print("So what would you like to do now?")
-print("1 - Go for a walk")
-print("2 - Eat Food")
-print("3 - Do nothing")
-var6 = input("Enter: ")
+    if old == True:
+        print(f"dang {d.age} is old!")
+    elif old == False:
+        print(f"yay {d.age} is young!")
 
-if var6 == 1:
-    path1()
+    print("Now that we're getting somewhere what would you like the color of my fur to be?")
+    var2 = input("Enter: ")
+    d.color = var2
+    print(f"{d.color}, is a nice color!")
+    print("ok a few more things, what would you like me to eat for dinner?")
+    var3 = input("Enter: ")
+    d.dinner = var3
+    print("What do you want be to eat for lunch?")
+    var4 = input("Enter: ")
+    d.lunch = var4
+    print("What do you want me to eat for breakfast?")
+    var5 = input("Enter: ")
+    d.breakfast = var5
+    print(f"so for dinner ill have {d.dinner} and for lunch ill have {d.lunch} and for breakfast ill have {d.breakfast} ")
+
+    print("So what would you like to do now?")
+    print("1 - Go for a walk")
+    print("2 - Eat Food")
+    print("3 - Do nothing")
+    var6 = input("Enter: ")
+
+    if var6 == 1:
+        path1()
+if __name__ == "__main__":
+    dogstart()
--- path1.py
+++ path1.py
@@ -1,16 +1,10 @@
-from main import Dog_data
+import main
 
-d = Dog_data("name", 1, "color", "food", "food", "food")
+d = main.Dog_data("name", 1, "color", "food", "food", "food")
+
 
 
 def path1():
-    d.get_name = d.name
-    d.get_age = d.age
-    d.get_color = d.color
-    d.get_lunch = d.lunch
-    d.get_dinner = d.dinner
-    d.get_breakfast = d.breakfast
-
     print("ok now while your on your walk you approch a dog what do you do?")
     print("1 - Bark at the dog")
     print("2 - Ignore the dog")
