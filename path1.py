f508e79b57c6761231fc4d0cea97ce2b667e718d
thecodepro90 <johnglopezzz@gmail.com>
2022-01-18 06:48:02 -0700

    idk

--- path1.py
+++ path1.py
@@ -1,10 +1,12 @@
-import main
-
-d = main.Dog_data("name", 1, "color", "food", "food", "food")
 
 
 
-def path1():
+def walkpath():
     print("ok now while your on your walk you approch a dog what do you do?")
     print("1 - Bark at the dog")
     print("2 - Ignore the dog")
+    var7 = input("Enter: ")
+
+    if var7 == 1:
+        for x in range(6):
+          print("BARK!")
