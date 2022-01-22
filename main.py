6bc38c1fecdb5c39676ddfc34013997688d78497
thecodepro90 <johnglopezzz@gmail.com>
2022-01-22 09:43:13 -0700

    idk

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from startstory import *
 from data.dogdata import *
 
 
-print("Started!")
-startdog()
-start_story()
+
+
+def tryagain():
+    print("Do you want to try again?")
+    print("1 - yes")
+    print("2 - no")
+    choice4 = input("Enter: ")
+    if choice4 =="1":
+        start()
+    elif choice4 == "2":
+        pass
+
+def start():
+    print("Started!")
+    startdog()
+    start_story()
+
+start()
+if ranback == False:
+    tryagain()
--- path/walkpath.py
+++ path/walkpath.py
@@ -1,3 +1,4 @@
+import random
 
 def walk_path():
     print("Ok so now on your walk you approch a dog he barks at you, what do you do?")
@@ -7,10 +8,43 @@ def walk_path():
     choice2 = input("Enter: ")
     if choice2 == "1":
         bark()
+    elif choice2 =="2":
+        print("You ran back home and the dog didnt chase you")
+        ranback = True
+
+    elif choice2 =="3":
+        ranvar = random.randint(0,2)
+
+        if ranvar == 1:
+            for i in range(4):
+                print("BARK")
+                print("SCRATCH")
+            print("You won, and killed the dog! and then ran back home")
+            ranback = True
+
+        elif ranvar == 2:
+            for i in range(4):
+                print("BARK")
+                print("SCRATCH")
+            print("the other dog won and you died")
+            ranback = False
 
 def bark():
     for i in range(7):
         print("BARK")
     print()
     print("The dog got mad and wants to fight! what do you do?")
-    print("")
+    print("1 - fight")
+    print("2 - run away")
+    choice3 = input("Enter: ")
+    if choice3 =="1":
+        ranback = False
+        for i in range(4):
+            print("BARK")
+            print("SCRATCH")
+        ranback = False
+        print("the dog won in the fight and you died")
+        ranback = False
+    elif choice3 =='2':
+        print("You ran back home and the dog didnt chase you")
+        ranback = True
