b1326f52a170bdfc4e39a93023e2819fde520e22
thecodepro90 <johnglopezzz@gmail.com>
2022-01-24 20:37:19 -0700

    update

--- path/eatpath.py
+++ path/eatpath.py
@@ -79,8 +79,37 @@ def lunch():
     choice10 = input("Enter: ")
     if choice10 =="1":
         lfastfood()
+    elif choice10 =="2":
+        print("At home you now get to choose what to eat so what would you like?")
+        choice12 = input("Enter: ")
+        d.lunch = choice12
+        for i in range(4):
+            print("Munch")
+        print(f"mmm {d.lunch} was good!")
+    elif choice12 =="3":
+        lfancyfood()
 
     def lfastfood():
         print("What do you want from the Lunch fast food place?")
         print("1 - Burger")
-        print("2 - ")
+        print("2 - double bacon burger")
+        print("3 - dino chicken nuggets")
+        choice11 = input("Enter: ")
+        if choice11 =="1":
+            for i in range(4):
+                print("Munch")
+            print("MMM that burger was SO GOOD lets go back home")
+        elif choice11 =="2":
+            for i in range(4):
+                print("Munch")
+            print("MMM that DOUBLE bacon burger was SO GOOD lets go back home")
+        elif choice11 =="3":
+            for i in range(4):
+                print("Munch")
+            print("MMM those DINO chichen NUGGETS were SO GOOD lets go back home")
+
+    def lfancyfood():
+        print("What do you want from the lunch fast food place?")
+        print("1 - Fauncy burgur")
+        print("2 - double bacon CHEESE burber")
+        print("3 - dino chicken nuggets with chik fila sauce")
